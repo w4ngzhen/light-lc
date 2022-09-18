7658f83e2a72ecc863b79b44e670ce9a386653c1
w4ngzhen <w4ngzhen@hotmail.com>
2022-09-18 17:31:25 +0800

    add: root index.ts for exporting all types and instance.

---
+++ packages/core/src/index.ts
@@ -0,0 +1,19 @@
+import 'antd/dist/antd.less';
+
+export type {
+    ElementNode
+} from './renderer/ElementNode';
+
+export type {
+    TypeRendererContext,
+    TypeRenderer
+} from './renderer/TypeRenderer';
+
+export {
+    TypeRendererManager
+} from './renderer/TypeRendererManager';
+
+export {
+    RenderEngine
+} from './renderer/RenderEngine';
+
